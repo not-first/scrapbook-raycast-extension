ef4d03d156a529af558ea69f16afa3e788267249
fakerr <111339712+not-first@users.noreply.github.com>
2024-07-19 21:19:44 +1000

    simple list display of posts

--- src/view-recent-posts.tsx
+++ src/view-recent-posts.tsx
@@ -1,11 +1,13 @@
 import { List } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
+import { Post } from "../lib/types";
 
 export default function ViewRecentPosts() {
-    const { isLoading, data, revalidate } = useFetch("https://scrapbook.hackclub.com/api/posts");
+  const { isLoading, data } = useFetch<Post[]>("https://scrapbook.hackclub.com/api/posts");
 
-    return (
-        <List></List>
-    )
-
+  return (
+    <List isLoading={isLoading}>
+      {data?.map((post: Post) => <List.Item key={post.id} title={post.text} subtitle={post.user.username} />)}
+    </List>
+  );
 }
