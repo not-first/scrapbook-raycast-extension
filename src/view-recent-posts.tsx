e836609b874677bc540ea94cc52379f8ef764687
fakerr <111339712+not-first@users.noreply.github.com>
2024-07-23 18:55:46 +1000

    add icons and slack link to metadata, markdown text display

--- src/view-recent-posts.tsx
+++ src/view-recent-posts.tsx
@@ -6,8 +6,28 @@ export default function ViewRecentPosts() {
   const { isLoading, data } = useFetch<Post[]>("https://scrapbook.hackclub.com/api/posts");
 
   return (
-    <List isLoading={isLoading}>
-      {data?.map((post: Post) => <List.Item key={post.id} title={post.text} subtitle={post.user.username} />)}
+    <List isLoading={isLoading} isShowingDetail>
+      {data?.map((post: Post) => (
+        <List.Item
+          key={post.id}
+          title={post.text}
+          subtitle={post.user.username}
+          detail={<List.Item.Detail markdown={
+            post.text + 
+            post.attachments.map(a => `![${"image"}](${a})`).join("\n")
+          }  metadata={
+            <List.Item.Detail.Metadata>
+                <List.Item.Detail.Metadata.Link title="Slack Link" target={post.slackUrl} text={"Open Slack"}/>
+                <List.Item.Detail.Metadata.Separator />
+                <List.Item.Detail.Metadata.TagList title="Reactions">
+                  {post.reactions.map(reaction => {
+                    return <List.Item.Detail.Metadata.TagList.Item key={reaction.name} icon={reaction.url}/>;
+                  })}
+                </List.Item.Detail.Metadata.TagList>
+              </List.Item.Detail.Metadata>
+          }/>}
+        />
+      ))}
     </List>
   );
 }
