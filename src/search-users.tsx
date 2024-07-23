3fdc14750281a252bb0500dd0595876cd6c7624c
fakerr <111339712+not-first@users.noreply.github.com>
2024-07-24 09:22:01 +1000

    more user information in search user command

--- src/search-users.tsx
+++ src/search-users.tsx
@@ -1,24 +1,33 @@
-import { Image, LaunchProps, List } from "@raycast/api";
+import { Icon, Image, LaunchProps, List } from "@raycast/api";
 import { useFetch } from "@raycast/utils";
 import { User } from "../lib/types";
-import { useState } from "react";
 
 export default function SearchUsers(props: LaunchProps) {
   const { isLoading, data } = useFetch<User[]>("https://scrapbook.hackclub.com/api/users");
-  const [enteredUsername, setEnteredUsername] = useState<string>(props.launchContext?.username);
 
   return (
-    <List isLoading={isLoading} searchText={enteredUsername} isShowingDetail>
+    <List isLoading={isLoading} searchText={props.launchContext?.username} isShowingDetail>
       {data?.map((user: User) => {
         return (
-          <List.Item 
+          <List.Item
             key={user.id}
             title={user.username}
             icon={{ source: user.avatar, mask: Image.Mask.Circle }}
             detail={
-                <List.Item.Detail.Metadata>
-                    
-                </List.Item.Detail.Metadata>
+              <List.Item.Detail metadata={
+                <List.Item.Detail.Metadata >
+                  <List.Item.Detail.Metadata.Label title="Name" icon={{source: user.avatar, mask: Image.Mask.Circle}} text={user.username} />
+                  <List.Item.Detail.Metadata.Label title="Email" icon={Icon.Envelope} text={user.email || "No email"} />
+                  <List.Item.Detail.Metadata.Link title="Website" text={user.website || "No website"} target={user.website || ""} />
+                  <List.Item.Detail.Metadata.Separator />
+                  <List.Item.Detail.Metadata.Link title="Github" text={user.github || "No Github"} target={user.github || ""} />
+                  <List.Item.Detail.Metadata.Link title="Scrapbook Profile" text={"Open Scrapbook"} target={ user.customDomain || "https://scrapbook.hackclub.com/" + encodeURIComponent(user.username)} />
+                <List.Item.Detail.Metadata.TagList title="IDs">
+                  <List.Item.Detail.Metadata.TagList.Item text={user.id} />
+                  <List.Item.Detail.Metadata.TagList.Item text={user.slackID || undefined} />
+                </List.Item.Detail.Metadata.TagList>
+              </List.Item.Detail.Metadata>
+              }/>
             }
           />
         );
